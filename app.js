8b3df83d7f103e56ea08ee2d6db3ef9e9a3daf6f
edertxodw <ederferte@gmail.com>
2016-12-28 21:54:42 +0100

    Nicknames y se puede desconectar de la sala

--- app.js
+++ app.js
@@ -3,14 +3,33 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var path = require('path');
+var nicknames = [];
 
 server.listen(3000);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.sockets.on('connection', function(socket){
+  socket.on('new user', function(data, callback){
+    if(nicknames.indexOf(data) != -1){
+      callback(false);
+    } else {
+      callback(true);
+      socket.nickname = data;
+      nicknames.push(socket.nickname);
+      updateNickNames();
+    }
+  });
   socket.on('send message', function(data){
     io.sockets.emit('new message', data);
     socket.broadcast.emit('new message', data);
   });
+  socket.on('disconnect', function(data){
+    if(!socket.nickname) return;
+    nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+    updateNickNames();
+  });
+  function updateNickNames(){
+    io.sockets.emit('usernames', nicknames);
+  }
 });
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,34 @@
 $(function($){
 
   var socket = io.connect();
+  var $nickForm = $('#setNick');
+  var $nickError = $('#nickError');
+  var $nickBox = $('#nickName');
+  var $users = $('#users');
   var $messageForm = $('#send-message');
   var $messageBox = $('#message');
   var $chat = $('#chat');
+
+  $nickForm.submit(function(e){
+    e.preventDefault();
+    socket.emit('new user', $nickBox.val(), function(data){
+      if(data){
+        $('#nick').hide();
+        $('#content').show();
+      } else {
+        $nickError.html('That username is already taken. Try again');
+      }
+    });
+    $nickBox.val('');
+  });
+
+  socket.on('usernames', function(data){
+    var html = "";
+    for(var i=0;i<data.length;i++){
+      html += data[i] + '<br/>';
+    }
+    $users.html(html);
+  });
 
   $messageForm.submit(function(e){
     e.preventDefault();
