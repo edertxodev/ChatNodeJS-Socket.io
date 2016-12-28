617106af1980c0f9eb31a857ef2a693b783bdf63
edertxodw <ederferte@gmail.com>
2016-12-28 20:43:37 +0100

    Primera prueba funcional

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var path = require('path');
 
 server.listen(3000);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.sockets.on('connection', function(socket){
+  socket.on('send message', function(data){
+    io.sockets.emit('new message', data);
+    socket.broadcast.emit('new message', data);
+  });
 });
