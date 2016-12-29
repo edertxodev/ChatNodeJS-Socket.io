8413b159cd2bd2e4bc85656de2aaa7c7cf26e116
edertxodw <ederferte@gmail.com>
2016-12-29 16:14:22 +0100

    Reordenando la aplicación

--- app.js
+++ app.js
@@ -2,94 +2,117 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-var mongoose = require('mongoose');
 var path = require('path');
+var bodyParser = require('body-parser');
 var users = {};
 
-server.listen(3000);
+/**
+ * Connect to DataBase
+ */
+require('./app/db.js');
 
-mongoose.connect('mongodb://edertxodw:abc123@ds145868.mlab.com:45868/chat-nodejs', function(err){
-  if(err){
-    console.log(err);
-  } else {
-    console.log('Connected to MongoDB');
-  }
-});
-
-var chatSchema = mongoose.Schema({
-  userid: Number,
-  nick: String,
-  msg: String,
-  created: {type: Date, default: Date.now}
-});
-
-var Chat = mongoose.model('Message', chatSchema);
+/**
+ * Get the model of Messages
+ */
+var Chat = require('./app/models/Message');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+/**
+ * Socket.io chat
+ */
 io.sockets.on('connection', function(socket){
-  Chat.find({}, function(err, docs){
-    if(err) throw err;
-    socket.emit('load old msgs', docs);
-  });
-  socket.on('new user', function(data, callback){
-    if(data in users){
-      callback(false);
-    } else {
-      callback(true);
-      socket.nickname = data;
-      users[socket.nickname] = socket;
-      updateNickNames();
-    }
-  });
-  socket.on('send message', function(data, callback){
-    var msg = data.trim();
-    if(msg.substr(0,3) === '/w '){
-      msg = msg.substr(3);
-      var index = msg.indexOf(' ');
-      if(index != -1){
-        var name = msg.substr(0, index);
-        var msg = msg.substring(index + 1);
-        if(name in users){
-          users[name].emit('whisper', {msg: msg, nick: socket.nickname})
-        } else {
-          callback('Error! Enter a valid user');
-        }
-      } else {
-        callback('Error! Please enter a message for your user');
-      }
-    } else {
-      var newMsg = new Chat({msg: msg, nick: socket.nickname});
-      newMsg.save(function(err){
-        if(err) throw err;
-        io.sockets.emit('new message', {msg: msg, nick: socket.nickname});
-      });
-    }
-  });
-  socket.on('disconnect', function(data){
-    if(!socket.nickname) return;
-    delete users[socket.nickname];
-    updateNickNames();
-  });
-  function updateNickNames(){
-    io.sockets.emit('usernames', Object.keys(users));
-  }
-});
+   Chat.find({}, function(err, docs){
+     if(err) throw err;
+     socket.emit('load old msgs', docs);
+   });
+   socket.on('new user', function(data, callback){
+     if(data in users){
+       callback(false);
+     } else {
+       callback(true);
+       socket.nickname = data;
+       users[socket.nickname] = socket;
+       updateNickNames();
+     }
+   });
+   socket.on('send message', function(data, callback){
+     var msg = data.trim();
+     if(msg.substr(0,3) === '/w '){
+       msg = msg.substr(3);
+       var index = msg.indexOf(' ');
+       if(index != -1){
+         var name = msg.substr(0, index);
+         var msg = msg.substring(index + 1);
+         if(name in users){
+           users[name].emit('whisper', {msg: msg, nick: socket.nickname})
+         } else {
+           callback('Error! Enter a valid user');
+         }
+       } else {
+         callback('Error! Please enter a message for your user');
+       }
+     } else {
+       var newMsg = new Chat({msg: msg, nick: socket.nickname});
+       newMsg.save(function(err){
+         if(err) throw err;
+         io.sockets.emit('new message', {msg: msg, nick: socket.nickname});
+       });
+     }
+   });
+   socket.on('disconnect', function(data){
+     if(!socket.nickname) return;
+     delete users[socket.nickname];
+     updateNickNames();
+   });
+   function updateNickNames(){
+     io.sockets.emit('usernames', Object.keys(users));
+   }
+ });
 
 /**
  * API REST
  */
-app.get('/get-messages', function(req, res){
-  Chat.find({}, function(err, docs){
-    if(err) throw err;
-    res.send(docs);
-  });
-});
+var router = express.Router();
 
-app.get('/new-message/', function(req, res){
-  var newMessage = new Chat({msg: req.msg, nick: req.nick});
-  newMsg.save(function(err){
-    if(err) throw err;
-    io.sockets.emit('new message', {msg: req.body.msg, nick: req.body.nick});
+  /**
+   * Get the chat template
+   */
+  router.route('/chat')
+    .get(function(req, res){
+      res.sendFile('public/index.html', {root: __dirname});
+    });
+
+  /**
+   * Get all messages
+   */
+  router.route('/get-messages')
+    .get(function(req, res){
+    Chat.find({}, function(err, docs){
+      if(err) throw err;
+      res.send(docs);
+    });
   });
-});
+
+  /**
+   * Create new message
+   */
+  router.route('/new-message')
+    .post(function(req, res){
+      var newMsg = new Chat();
+      newMsg.msg = req.body.msg;
+      newMsg.nick = req.body.nick;
+
+      newMsg.save(function(err){
+        if(err){
+          res.send(err);
+        }
+        res.json({message: 'Message created!'});
+      });
+  });
+
+app.use('/api', router);
+
+server.listen(8080);
